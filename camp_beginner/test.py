49a659f8699f12ece05c3e64e4cdf77a249e0234
zolevs <zolevs@gmail.com>
2025-10-09 23:48:07 +0200

    commit

---
+++ camp_beginner/test.py
@@ -0,0 +1,14 @@
+# ternary 
+# age = 21
+
+# def is_adult(age):
+#     return True if age > 18 else False
+
+# string operators
+string_test = "test"
+print("zole".upper())
+print("zole".lower())
+print("zole".title())
+print("zole".islower())
+print(len(string_test))
+print("st" in string_test)
